b8513230d56a6ee3b1e90952e2778c0425262150
ahu-bioinf-lab <130021909+ahu-bioinf-lab@users.noreply.github.com>
2024-09-24 18:40:15 +0800

    Update CMOMO_task1.py

--- CMOMO_task1.py
+++ CMOMO_task1.py
@@ -7,10 +7,11 @@ from tensorboardX import SummaryWriter
 import time
 
 from sub_code.models import CDDDModel
-from sub_code.NSGA2 import *
+from sub_code.fitness import *
 from sub_code.property import *
 from warnings import simplefilter
-from sub_code.archive import *
+from sub_code.generation_rule import *
+from sub_code.selection_rule import *
 simplefilter(action="ignore", category=FutureWarning)
 import pygmo as pg
 import math
@@ -41,31 +42,20 @@ if __name__ == "__main__":
         fitness = fitness_qeddrd
         tres = [0.8, 0.3, 0.4]
         col = ['SMILES', 'mol_id', 'qed', 'sim', 'drd', 'CV']
-    elif args.opt == 'qedsajnk':
-        ff = ff_qedjnksa
-        fitness = fitness_qedjnksa
-        tres = [0.8, 0.5, 0.7, 0.3]
-        col = ['SMILES', 'mol_id', 'qed', 'jnk3', 'sa_nom', 'sim']
     elif args.opt == 'qedsagsk':
         ff = ff_qedgsksa
         fitness = fitness_qedgsksa
         tres = [0.8, 0.5, 0.7, 0.3]
         col = ['SMILES', 'mol_id', 'qed', 'gskb', 'sa_nom', 'sim']
-    elif args.opt == 'qedsadrd':
-        ff = ff_qeddrdsa
-        fitness = fitness_qeddrdsa
-        tres = [0.8, 0.5, 0.7, 0.3]
-        col = ['SMILES', 'mol_id', 'qed', 'drd2', 'sa_nom', 'sim']
     args = parser.parse_args()
     # device = 'cuda'
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print('device:', device)
 
-    ######加载预训练的JTVAE
+    ######loading the pre-trained encoder-decoder
     model = CDDDModel()
-    # canonicalize
+    # loading dataset
     data = pd.read_csv('./data/qedplogp_test.csv').values
-
     simbank = pd.read_csv('./data/archive/qedplogp_simbank_100.csv').values
     smi_iter_last = []  
     HV_pop = []
@@ -74,20 +64,21 @@ if __name__ == "__main__":
     num_feasible = [] 
     num_feasible_arc = [] 
     runtime = []
-    nPop = 100  # 种群个体数
-    pc = 1  # 交叉率
-    pm = 0.5  # 变异率
-    nChr = 512  # 染色体长度
-    lb = -1  # 下界
-    rb = 1  # 上界
-    numberOfGroups = 16
-    d = 0.26  # 线性交叉参数
+    # parameter settings
+    nPop = 100  # the size of population
+    pc = 1  # crossover rate
+    pm = 0.5  # mutation rate
+    nChr = 512  # length of vectors
+    lb = -1  # lower bound
+    rb = 1  # upper bound
+    numberOfGroups = 16 #number of groups in mutation
+    d = 0.26  # the parameter in crossover
     disM = 20
-    nIter = 100  # 迭代次数
-    nIter_1  = 50  # 迭代次数
+    nIter = 100  # the total number of evolution generation
+    nIter_1  = 50  # evolution generation in stage 1
     restart = 1
-    m = 5  # 变异个数
-    SR = 0  # 记录优化成功的分子数0.9，0.4
+    SR = 0
+    # the ID of lead molecules in dataset
     a = list(range(0, 20, 20))  # 180
     b = list(range(20, 100, 20))  # 200
     c = 2  # 几条约束，计算约束违反度
@@ -95,8 +86,9 @@ if __name__ == "__main__":
     for num in range(len(a)):
         mm1 = a[num]
         mm2 = b[num]
-        pop_smis_iter = [['SMILES', 'mol_id', 'iter', 'QED', 'sim', 'pogp_imp','CV']]  # 分批存储每一代进化属性值，CV
+        pop_smis_iter = [['SMILES', 'mol_id', 'iter', 'QED', 'sim', 'pogp_imp','CV']]
         archive_smis_iter = [['SMILES', 'mol_id', 'iter', 'QED', 'sim', 'pogp_imp','CV']]
+        # the optimization of each lead molecule
         for i in range(mm1,mm2):
             #num = num +1
             nn = i
@@ -111,29 +103,30 @@ if __name__ == "__main__":
             results_dir = os.path.join('results', args.seq)
             os.makedirs(results_dir, exist_ok=True)
             writer = SummaryWriter(os.path.join('runs', args.seq, run_str))
-            z_0 = model.encode(smiles)  # 对分子序列进行编码
-            fp_0 = morgan_fingerprint(Chem.MolFromSmiles(seq))  # 分子序列的摩根指纹，用于计算相似性
+            z_0 = model.encode(smiles)  # the encoding of the lead
+            fp_0 = morgan_fingerprint(Chem.MolFromSmiles(seq))
             fits_0 = ff_qedlogp(seq, mol_0, fp_0)
             print(fits_0)
-            #simbank嵌入
+            # the embedding of molecules inthe bank library
             aa = simbank[:, 1] == i
             index = np.where(aa == 1)
-            simbank_i = simbank[index]  # 提取指定分子的初始种群
+            simbank_i = simbank[index]
             num_int = len(simbank_i)
             bank_emb = np.zeros((num_int, 512))
             for i in range(num_int):
                 bank_emb[i] = model.encode(simbank_i[i][0])
             bank_emb = torch.tensor(bank_emb).to(torch.float32)
-            ########### 隐向量的扰动数量、控制相似性的系数，越大生成的相似性越低，容易产生空集
-            r = 1  # 重启
-            while r <= restart:
-                hv_pop = np.zeros(nIter)#初始第一阶段值为0
-                hv_arc = []
-                num_feasible_i = []  # 储存当前分子种群可行解个数
-                num_feasible_arc_i = np.zeros(nIter + 1)
-                t1 = time.time()  # 开始时间
 
-                # bank库
+            ########### optimization process
+            r = 1  # the time of re-optimization
+            while r <= restart:
+                hv_pop = np.zeros(nIter)
+                hv_arc = []
+                num_feasible_i = []
+                num_feasible_arc_i = np.zeros(nIter + 1)
+                t1 = time.time()
+
+                # Population initialization
                 bankpop = crossover_z0_2(z_0, bank_emb, pc, d, lb, rb)
                 bankpop = torch.tensor(bankpop).to(torch.float32)
                 # 解码评估
@@ -144,35 +137,33 @@ if __name__ == "__main__":
                 CV_bank = CV(bankmol, c)
 
                 pops, fits, CV_pops, smis = select1_uni(nPop, bankpop, bankfits, CV_bank, banksmiles)
-                num_feasible_i.append(CV_pops.count(0))  # 初始种群的可行解个数
-                num_feasible_arc_i[0] = CV_pops.count(0) # 初始种群的可行解个数和档案相同
-                print('初始种群')
+                num_feasible_i.append(CV_pops.count(0))  # number of feasible molecules in the initial population
+                num_feasible_arc_i[0] = CV_pops.count(0)
+                print('the initial population')
 
                 iter = 0
                 Archive_pops = np.zeros((1, 1))
                 Archive_fits = np.zeros((1, 1))
                 Archive_CV = np.zeros((1, 1))
                 Archive_smi = np.zeros((1, 1))
+                # optimization generation
                 while iter < nIter:
                     # 进度条
                     print("【进度】【{0:20s}】【正在进行{1}代...】【共{2}代】". \
                           format('▋' * int(iter / nIter * 20), iter, nIter), end='\r')
-                    #交叉变异
-                    #print(fits)
+                    #optimization stage 1
                     if iter < nIter_1:
-                        # 进化多个子代2N
-                        print('stage1进化子代')
-                        chrpops = crossover_2(pops, pc, d, lb, rb)  # 混合线性交叉
-                        #chrpops = mutate(chrpops, pm, nChr, m)  # 变异产生子种群
+                        print('stage1')
+                        #generate offspring molecules
+                        chrpops = crossover_2(pops, pc, d, lb, rb)
                         chrpops = group_mut(chrpops,lb, rb, pm, numberOfGroups)
-                        # 评估子代属性值
+                        # evaluate fitness
                         chrpops = torch.from_numpy(chrpops)
-                        chrmol, chrsmiles = model.decode(chrpops)  ###解码潜在表示，分子和SMILES
+                        chrmol, chrsmiles = model.decode(chrpops)  ###decoding
                         chrfits = fitness_qedlogp(chrsmiles, chrmol, fp_0)
                         chrfits[np.isnan(chrfits)] = -20
-                        # 更新Archive，不重复
-                        print('stage1更新archive')
-                        CV_offspring = CV(chrmol, c)  # 评估子代CV值
+                        print('stage1 update archive')
+                        CV_offspring = CV(chrmol, c)
                         MergePops = np.concatenate((pops, chrpops), axis=0)
                         MergeFits = np.concatenate((fits, chrfits), axis=0)
                         MergeCV = np.concatenate((CV_pops, CV_offspring), axis=0)
@@ -183,10 +174,9 @@ if __name__ == "__main__":
                                                                                              Archive_CV, Archive_smi,
                                                                                              iter)
                         num_feasible_arc_i[iter+1] = np.count_nonzero(Archive_CV == 0)
-                        # 更新种群，不重复
-                        print('stage1更新population')
+                        print('stage1 update population')
                         pops, fits, CV_pops, smis = optSelect_uni(pops, fits, CV_pops, smis, chrpops, chrfits, CV_offspring, chrsmiles, nPop)
-                        num_feasible_i.append(CV_pops.count(0))  # 初始种群的可行解个数
+                        num_feasible_i.append(CV_pops.count(0))
                         ###save smiles in population at each iteration
                         for i in range(len(smis)):
                             tuple = [smis[i], nn, iter, fits[i][0], fits[i][2],fits[i][1]-fits_0[1], CV_pops[i]]
@@ -195,7 +185,7 @@ if __name__ == "__main__":
                         for i in range(len(Archive_smi)):
                             tuple = [Archive_smi[i], nn, iter, Archive_fits[i][0], Archive_fits[i][2],Archive_fits[i][1]-fits_0[1], Archive_CV[i]]
                             archive_smis_iter.append(tuple)
-                        ###save cHV, cPD in stage 1,先找出可行解属性
+                        ###save cHV, cPD in stage 1
                         fits_c = fits[np.where(np.array(CV_pops)==0)]
                         try:
                             dominated_hypervolume = pg.hypervolume(np.array(
@@ -217,41 +207,39 @@ if __name__ == "__main__":
                             pure_div = 0
                         hv_arc.append((dominated_hypervolume))
 
-                        # Stage 2:
+                        # #optimization stage 2
                     else:
-                        # 档案库开始进化
-                        print('stage2更新子代')
-                        num_feasible_i.append(np.count_nonzero(Archive_CV == 0))#初始档案时的可行解个数存储
-                        chrpops = crossover_2(Archive_pops, pc, d, lb, rb)  # 混合线性交叉
-                        #chrpops = mutate(chrpops, pm, nChr, m)  # 变异产生子种群
+                        print('stage2')
+                        #generate offspring molecules
+                        num_feasible_i.append(np.count_nonzero(Archive_CV == 0))
+                        chrpops = crossover_2(Archive_pops, pc, d, lb, rb)
                         chrpops = group_mut(chrpops,lb, rb, pm, numberOfGroups)
-                        # 评估子代属性值
+                        # calculate fitness
                         chrpops = torch.from_numpy(chrpops)
-                        chrmol, chrsmiles = model.decode(chrpops)  ###解码潜在表示，分子和SMILES
+                        chrmol, chrsmiles = model.decode(chrpops)
                         chrfits = fitness_qedlogp(chrsmiles, chrmol, fp_0)
                         chrfits[np.isnan(chrfits)] = -20
                         CV_offspring = CV(chrmol, c)
-                        # 合并父代种群和子代种群构成一个新种群
+                        # merge parent and offspring molecules
                         MergePops = np.concatenate((Archive_pops, chrpops), axis=0)
                         MergeFits = np.concatenate((Archive_fits, chrfits), axis=0)
                         MergeCV = np.concatenate((Archive_CV, CV_offspring), axis=0)
                         Mergesmiles = np.concatenate((Archive_smi, chrsmiles), axis=0)
-                        # 首先去除重复
                         MergeFits_uni, indices = np.unique(MergeFits, axis=0, return_index=True)
                         MergePops_uni = MergePops[indices]
                         MergeCV_uni = MergeCV[indices]
                         Mergesmiles_uni = Mergesmiles[indices]
                         # Rank1
-                        print('stage2排序1')
+                        print('stage2 rank1')
                         S1 = score1_nsga2(MergePops_uni, MergeFits_uni)
                         # Rank2
-                        print('stage2排序2')
+                        print('stage2  rank2')
                         S2 = score2_CDP(MergePops_uni, MergeFits_uni, MergeCV_uni)
-                        # Score_all,排名前N个分子进入archive，排序索引
+                        # Score_all, select molecules
                         alpha = math.cos((iter - nIter_1) / (nIter - nIter_1) * math.pi)
                         S_all = 0.5 * (1 + alpha) * S1 + (1 - 0.5 * (1 + alpha)) *S2 
                         sortedID = np.argsort(S_all) 
-                        print('stage2更新population')
+                        print('stage2 update population')
                         Archive_pops = MergePops_uni[sortedID[0:nPop]]
                         Archive_fits = MergeFits_uni[sortedID[0:nPop]]
                         Archive_CV = MergeCV_uni[sortedID[0:nPop]]
@@ -274,17 +262,17 @@ if __name__ == "__main__":
                     iter = iter + 1
                     print(iter)
                         
-                #解码最终种群分子
+                #the last population
                 endsmiles = np.array(Archive_smi)
                 CV_endmol = Archive_CV
                 endfits = Archive_fits
-                # 最后一代约束pareto前沿上的分子
+                # Pareto molecules in the last population
                 ranks = CDP_simple(endsmiles, endfits, CV_endmol)  # 约束非支配排序
                 paretoendsmiles = endsmiles[ranks == 0]
                 paretoFits = endfits[ranks == 0]
                 paretoCV = CV_endmol[ranks == 0]
-                # 判断是否优化成功
-                rr = []  # 储存endfits是否满足>=0.9,>=0.4，True=1，False=0
+                # the optimized molecules
+                rr = []
                 unique_smiles = []
                 for i in range(len(paretoendsmiles)):
                     if paretoCV[i] == 0:
@@ -301,8 +289,8 @@ if __name__ == "__main__":
                 else:
                     r = r + 1
                 print('restart:', r)
-                t2 = time.time()  # 一个分子重启动进化后时间
-                time_1 = (t2 - t1) / 60  # 扰动个数
+                t2 = time.time()
+                time_1 = (t2 - t1) / 60
                 print('run time:', time_1)
                 runtime.append(time_1)
 
